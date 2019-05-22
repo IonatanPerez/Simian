ba37146772e2a0948186a70eefa810d50606ea74
unknown <ionatan@gmail.com>
2019-05-22 00:27:21 -0300

    achivos trabajados en desktop casa

---
+++ testTwitter.py
@@ -0,0 +1,6 @@
+import tweepy #The Twitter API
+from tkinter import * #For the GUI
+from time import sleep
+from datetime import datetime
+from textblob import TextBlob #For Sentiment Analysis
+import matplotlib.pyplot as plt #For Graphing the Data
