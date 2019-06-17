c11b6244d59b52888141a6abaf2f9f794ebe8f42
ionatan <ionatan@gmail.com>
2019-06-17 17:07:52 -0300

    Estuve revisando un poco la estructura y quiero modificar el diagrama de clases que estaba quedando muy enredado. Empece por definir una clase abstracta que es lo que hice hasta ahora

---
+++ abstract.py
@@ -0,0 +1,22 @@
+import IPython.core.oinspect as insp
+
+print ("Recuerde que para que esta clase funcione debe incluir self.chequeosdeimplementacion() en caso de sobreescrbir el __init__")
+
+class Abstract:
+   
+    def __init__ (self):
+        self.chequeosdeimplementacion()
+        
+    def chequeosdeimplementacion(self):
+        metodos = [method for method in dir(self) if callable(getattr(self, method))]
+        metodos = [metodo for metodo in metodos if not metodo[0:2]=="__"]
+        propiedades = [method for method in dir(self) if not callable(getattr(self, method))]
+        propiedades = [propiedad for propiedad in propiedades if not propiedad[0:2]=="__"]
+        if metodos:
+            for metodo in metodos:
+                linea = insp.getsource(getattr(self, metodo)).split("\n")[1].lstrip()
+                if linea == "return \"Implementar\"":
+                    print ("Warning: En " +type(self).__name__+" no se implemento el metodo " + metodo)
+        if propiedades:
+            for propiedad in propiedades:
+                if getattr(self, propiedad) == "Implementar": print ("Warning: En " +type(self).__name__+" no se implemento la propiedad " + propiedad)
