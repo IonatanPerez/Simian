3512ad56a010823eea258eefadbe4b69932cd576
ionatan <ionatan@gmail.com>
2019-10-28 23:40:07 -0300

    Casi terminado , estoy en la parte del batch. Hago sync

--- Selenium/AutoresLN.py
+++ Selenium/AutoresLN.py
@@ -9,10 +9,9 @@ import os
 
 # settings
 localfolder = "./Selenium/raw/"
-authorsFile = localfolder + "authors.json"
 scrappingFile = localfolder + "scrappingLog.json"
-noAutoresLimite = 10 # Asumimos que cuando hay 10 ids consecutivos sin info es que se acabo la lista de autores. 
-
+nroAutoresLimite = 10 # Asumimos que cuando hay 10 ids consecutivos sin info es que se acabo la lista de autores. 
+batchSize = 10 # Cuando se junto la info de 10 autores se vuelca a disco en un archivo aparte.
 
 
 # Pensamos en la estructura de la info
@@ -31,7 +30,7 @@ noAutoresLimite = 10 # Asumimos que cuando hay 10 ids consecutivos sin info es q
     # FotoId (INT) -- Id de la foto para eventualmente identificar imagenes descargadas. LN usa un servidor de fotos 
     #                 con un formato que es el siguiente https://bucket[1,2,3].glanacion.com/anexos/fotos/[id[-2:]]/[id]h[altoenpixeles].jpg
     # Notas (LST) -- Listado de notas, aca hay que ver todavia que estructura van a tener la info de las notas, por ahora van las URL
-
+    # NotasEncontradas (INT) -- Cantidad de notas encontradas
 
 
 # La nacion usa un codigo interno que ignora el nombre del autor, lo que le importa es el ultimo numero
@@ -40,7 +39,7 @@ noAutoresLimite = 10 # Asumimos que cuando hay 10 ids consecutivos sin info es q
 
 
 # Inicializamos ciertas variables 
-noAutoresAcumulados = 0 # Numero de ids sin info consecutiva
+nroAutoresAcumulados = 0 # Numero de ids sin info consecutiva
 
 # Iniciamos las variables relacionadas al scrapping porque es comun que se corte y queremos poder retomar.
 if os.path.exists(scrappingFile):
@@ -50,6 +49,11 @@ else:
     infoScrapping = {}
     infoScrapping["nextId"] = 1
 id = infoScrapping["nextId"]
+
+# Actualizamos el nombre del archivo de autores
+authorsFile = localfolder + "authors" + str((id//10)*10) + ".json"
+ 
+
 # Cargamos la info ya guardada de autores
 if os.path.exists(authorsFile):
     with open(authorsFile) as json_file:
@@ -61,15 +65,15 @@ else:
 # al caracter dinamico de la pagina, si se corta graba el avance.
 try:
     driver = webdriver.Firefox()
-    while noAutoresAcumulados < noAutoresLimite:
+    while nroAutoresAcumulados < nroAutoresLimite:
         infoAutor = {}
         url = "https://www.lanacion.com.ar/autor/id-"+str(id)
         driver.get(url)
         assert "Autor" in driver.title
         if driver.title != "Autor - - LA NACION":
-            noAutoresAcumulados = 0
+            nroAutoresAcumulados = 0
         else:
-            noAutoresAcumulados =+ 1
+            nroAutoresAcumulados =+ 1
             id = id +1 
             continue
         # Extraemos el nombre del autor
@@ -99,20 +103,92 @@ try:
             while more:
                 try:
                     elem.click()
+                    numeroDeScrolls = 0
                 except Exception as ex:
                     if type(ex).__name__ == "ElementNotInteractableException":
                         more = False
                     else:
-                        raise
-            print (infoAutor)
+                        if type(ex).__name__ == "ElementClickInterceptedException":
+                            elem.location_once_scrolled_into_view
+                        else:
+                            print (type(ex).__name__)
+                            raise
+
+
+        # Una vez cargada la pagina completa tratamos de extraer el listado de notas.
+        # Al parecer la estructura de nombre es similar a la de autores en el sentido de 
+        # que lo que importa es el id y el resto del link es irrelevante, excepto por la 
+        # seccion, eso hace que en principio no se pueda buscar en forma secuencial. Sin 
+        # embargo si recolectamos la lista de todas las secciones posibles, eventualmente
+        # se podria probar con el id en todas las secciones.
+
+        # Buscamos el cuerpo de notas
+        listado = driver.find_element_by_class_name("listado")
+        notas = listado.find_elements_by_class_name("nota")
+
+        
+        # Estrucrtura de la info que queremos recolectar:
+
+        # Id (INT) -- Id de la nota
+        # IdAutor (INT) -- Id del autor de la nota
+        # Link (STR) -- Link a la nota
+        # Seccion (STR) -- Seccion, esto es importante para poder saber que secciones hay y testear urls de notas
+        # Tema (STR) -- String del tema
+        # TemaId (INT) -- Id del tema, los temas al igual que los demas se pueden buscar por fuerza bruta 
+        #                   buscando https://www.lanacion.com.ar/tema/test-tid[ID]
+        # Imagen (STR) -- Link a la imagen si la hay
+        # ImagenId (INT) -- Si lo hay
+        # Fecha (STR) -- Viene como STR pero supongo que despues se puede pasar a algo mas procesable
+
+        infoAutor["InfoNotas"] = []
+        infoAutor["NotasEncontradas"] = len(notas)
+        for nota in notas:
+            InfoNota = {}  
+            InfoNota["IdAutor"] = id
+            InfoNota["Link"] = nota.find_element_by_tag_name("h2").find_element_by_tag_name("a").get_attribute("href")
+            assert InfoNota["Link"].split("-")[-1][:3] == "nid" # Chequeamos que este leyendo el id de la nota
+            InfoNota["Id"] = InfoNota["Link"].split("-")[-1][3:]
+            InfoNota["Seccion"] = "/".join(InfoNota["Link"].split("/")[3:][:-1])
+            if nota.find_elements_by_tag_name("h3"):
+                InfoNota["Tema"] = nota.find_element_by_tag_name("h3").text
+                InfoNota["TemaId"] = nota.find_element_by_tag_name("h3").find_element_by_tag_name("a").get_attribute("href").split("-")[-1]
+            else:
+                InfoNota["Tema"] = None
+                InfoNota["TemaId"] = None
+            if nota.find_elements_by_tag_name("img"):
+                InfoNota["Imagen"] = nota.find_element_by_class_name("figure").find_element_by_tag_name("img").get_attribute("src")
+                InfoNota["ImagenId"] = InfoNota["Imagen"].split("/")[-1].split("h")[0]
+            else:
+                InfoNota["Imagen"] = None
+                InfoNota["ImagenId"] = None
+            if nota.find_elements_by_class_name("fecha"):
+                InfoNota["Fecha"] = nota.find_element_by_class_name("fecha").text
+            else:
+                InfoNota["Fecha"] = None
+            infoAutor["InfoNotas"].append(InfoNota)
+
+        print (infoAutor)
         infoAutores.append(infoAutor)
+
+
+        # Si acumulamos 10 autores hacemos el volcado a disco y limpiamos la lista
+        if id//batchSize == 0:
+            print ("Volcando a disco el archivo" + authorsFile)
+            with open(authorsFile, 'w') as fp:
+                json.dump(infoAutores, fp)
+            infoAutores = []
+            authorsFile = localfolder + "authors" + str((id//10)*10) + ".json"
+
         infoScrapping["nextId"] = id + 1
         id = id + 1                  
         with open(scrappingFile, 'w') as fp:
-            json.dump(infoScrapping, fp)     
+            json.dump(infoScrapping, fp)
+        
+    print ("ejecucion terminada con exito")
     with open(authorsFile, 'w') as fp:
         json.dump(infoAutores, fp)     
 except:
+    print ("Final inesperado")
     with open(authorsFile, 'w') as fp:
         json.dump(infoAutores, fp)     
     with open(scrappingFile, 'w') as fp:
